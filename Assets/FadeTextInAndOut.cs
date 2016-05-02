c9ae0197c0b7e3b0e374fec6c0523fdd46e3b135
Jason Cheung <jayson.h.cheung@gmail.com>
2016-05-01 21:54:42 -0700

    Prologue - Incomplete
    Background and sprites implemented, missing animations and scripts.

---
+++ Assets/FadeTextInAndOut.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeTextInAndOut : MonoBehaviour {
+
+	Animator _animator;
+
+	// Use this for initialization
+	void Start () {
+		// Animator animation = GetComponent<Animator> ();
+		_animator = GetComponent<Animator> (); 
+		PlayOneShot ("AnimStart");
+		/*
+		animation["FadeTextInAndOut"].wrapMode = WrapMode.Once;
+		animation.Play("FadeTextInAndOut");
+*/
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public IEnumerator PlayOneShot ( string paramName )
+	{
+		_animator.SetBool( paramName, true );
+		yield return null;
+		_animator.SetBool( paramName, false );
+	}
+}
---
+++ Assets/PrologueHeroAnimation.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrologueHeroAnimation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// PlayAnimation ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	/*
+	public IEnumerator PlayAnimation()
+	{
+		yield return WaitForSeconds(2);
+		transform.localScale = new Vector3 (1f, 1f, 1f);
+		yield return WaitForSeconds(5);
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (10, 0);
+	}
+	*/
+}
